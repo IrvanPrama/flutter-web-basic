140f8757515fda12e97126014c742b1d96e2e83e
IrvanPrama <bluxfusion@gmail.com>
2021-07-15 20:04:11 +0800

    join button

--- lib/widgets/call_to_actions/CallToActions.dart
+++ lib/widgets/call_to_actions/CallToActions.dart
@@ -6,16 +6,19 @@ class CallToActions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-      child: Text(
-        title,
-        style: TextStyle(
-            fontSize: 18, fontWeight: FontWeight.w800, color: Colors.white),
+    return MaterialButton(
+      onPressed: () {},
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+        child: Text(
+          title,
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.w800, color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 31, 229, 146),
+            borderRadius: BorderRadius.circular(5)),
       ),
-      decoration: BoxDecoration(
-          color: Color.fromARGB(255, 31, 229, 146),
-          borderRadius: BorderRadius.circular(5)),
     );
   }
 }
